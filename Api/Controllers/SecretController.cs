42da675080d693c92a839ed6fbe8d2f8274b6028
kurt <feeleyfire@gmail.com>
2022-01-30 13:12:52 -0500

    initial commit

---
+++ Api/Controllers/SecretController.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Amazon.SecretsManager;
+using Amazon.SecretsManager.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SecretController : ControllerBase
+    {
+        private readonly IAmazonSecretsManager _secretsManager;
+
+        public SecretController(IAmazonSecretsManager secretsManager)
+        {
+            _secretsManager = secretsManager;
+        }
+
+        [HttpGet]
+        public IActionResult GetSecret()
+        {
+            GetSecretValueRequest request = new GetSecretValueRequest();
+            request.SecretId = "test-secret";
+            request.VersionStage = "AWSCURRENT";
+
+            Task<GetSecretValueResponse> response = _secretsManager.GetSecretValueAsync(request);
+
+            return Ok(new { Secret = response.Result.SecretString });
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# secretsmanager
+# Secrets Manager
+This repo contains example code for the blog post at: 
+
+https://awswith.net/2022/02/01/securing-net-app-secrets-with-aws-secrets-manager/

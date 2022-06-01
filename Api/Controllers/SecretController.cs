a4f76f0ee330d0d091e748a3cf1a41542b741d9d
awswithdotnet <74926017+awswithdotnet@users.noreply.github.com>
2022-06-01 17:39:40 -0400

    initial commit

--- Api/Controllers/SecretController.cs
+++ Api/Controllers/SecretController.cs
@@ -16,16 +16,14 @@ namespace Api.Controllers
             _secretsManager = secretsManager;
         }
 
-        [HttpGet]
-        public IActionResult GetSecret()
-        {
-            GetSecretValueRequest request = new GetSecretValueRequest();
-            request.SecretId = "test-secret";
-            request.VersionStage = "AWSCURRENT";
-
-            Task<GetSecretValueResponse> response = _secretsManager.GetSecretValueAsync(request);
-
-            return Ok(new { Secret = response.Result.SecretString });
-        }
+       [HttpGet]
+       public async Task<IActionResult> GetSecret()
+       {
+           GetSecretValueRequest request = new GetSecretValueRequest();
+           request.SecretId = "test-secret";
+           request.VersionStage = "AWSCURRENT";
+           GetSecretValueResponse response = await  _secretsManager.GetSecretValueAsync(request);
+           return Ok(new { Secret = response.SecretString });
+       }
     }
 }
